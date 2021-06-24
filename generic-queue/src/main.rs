14572f36a2e85a7c760d3f05ff4f6c4d80893bdb
nick@deploy.at <mikowhy@tutanota.com>
2021-06-24 21:16:43 +0200

    tests

--- generic-queue/src/main.rs
+++ generic-queue/src/main.rs
@@ -42,3 +42,52 @@ impl<T> Queue<T> {
         (self.older, self.younger)
     }
 }
+
+#[test]
+fn test() {
+    let mut q = Queue::new();
+
+    q.push('*');
+    assert_eq!(q.pop(), Some('*'));
+    assert_eq!(q.pop(), None);
+
+    q.push('0');
+    q.push('1');
+    assert_eq!(q.pop(), Some('0'));
+
+    q.push('∞');
+    assert_eq!(q.pop(), Some('1'));
+    assert_eq!(q.pop(), Some('∞'));
+    assert_eq!(q.pop(), None);
+
+    assert!(q.is_empty());
+    q.push('☉');
+    assert!(!q.is_empty());
+    q.pop();
+    assert!(q.is_empty());
+
+    let mut q = Queue::new();
+
+    q.push('P');
+    q.push('D');
+    assert_eq!(q.pop(), Some('P'));
+    q.push('X');
+
+    assert_eq!(q.split(), (vec!['D'], vec!['X']));
+}
+
+#[test]
+fn test_generic() {
+    let mut q = Queue::<char>::new();
+    &mut q;
+    drop(q);
+
+    let mut q = Queue::new();
+    let mut r = Queue::new();
+
+    q.push("CAD");  // apparently a Queue<&'static str>
+    r.push(0.74);   // apparently a Queue<f64>
+
+    q.push("BTC");   // Bitcoins per USD, 2019-6
+    r.push(13764.0); // Rust fails to detect irrational exuberance
+}
